32641406eb0b67f4abfb8da5d646c0e08c943193
Malik Kasmi <malik.kasmi@gmail.com>
2016-12-02 14:58:00 +0100

    work on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,18 @@
 Team.destroy_all
 User.destroy_all
 
-captain_paul = User.create!(username: "paul", email: "paul@captain.com", password: "123456")
-captain_malik = User.create!(username: "malik", email: "malik@captain.com", password: "789456")
-captain_test = User.create!(username: "test", email: "test@captain.com", password: "1478541")
+paul = User.create!(username: "paul", email: "paul@team8.com", password: "paul123")
+malik = User.create!(username: "malik", email: "malik@team8.com", password: "malik123")
+jordan = User.create!(username: "jordan", email: "jordan@team8.com", password: "jordan123")
+johnson = User.create!(username: "johnson", email: "johnson@team8.com", password: "johnson123")
+bob = User.create!(username: "bob", email: "bob@team8.com", password: "bob123")
 
 
-
-team_1 = Team.new(name: "team1", city: "Paris", description: "Super equipe", captain: captain_paul)
+team_1 = Team.new(name: "Paul's team", city: "Paris", description: "Super equipe", captain: paul)
 team_1.save!
 
-team_2 = Team.new(name: "team2", city: "Nantes", description: "cool", captain: captain_malik)
+team_2 = Team.new(name: "Malik's team", city: "Lyon", description: "Won 6-0 against Nantes", captain: malik)
 team_2.save!
 
-team_3 = Team.new(name: "team3", city: "Angers", description: "ok", captain: captain_test)
+team_3 = Team.new(name: "Chicago Bulls", city: "Chicago", description: "Best team ever", captain: jordan)
 team_3.save!
