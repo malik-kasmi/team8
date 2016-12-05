201fe35e080b49bc726ed8958bda30e95b4a8910
Malik Kasmi <malik.kasmi@gmail.com>
2016-12-05 15:14:33 +0100

    create model Game + method games

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,13 @@
 class Team < ApplicationRecord
   belongs_to :captain, class_name: "User", foreign_key: 'captain_id'
   has_many :team_users
+  has_many :game_1s, class_name: "Game", foreign_key: :team1_id
+  has_many :game_2s, class_name: "Game", foreign_key: :team2_id
+  has_many :wins, class_name: "Game", foreign_key: :winner_id
+
+  def games
+    games_array = self.game_1s + self.game_2s
+    games_array.compact
+  end
+
 end
