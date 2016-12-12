d791693d7c3d9970eb30911192064651eb1a788a
Paul de Vaublanc <pauldevaublanc@gmail.com>
2016-12-12 12:20:31 +0100

    front team index 2

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,14 @@
 class GamesController < ApplicationController
 
- def index
-    if params[:city].blank?
-      @teams = Team.all
-    else
-      @teams = Team.where(city: params[:city].capitalize)
-    end
-  end
-
   def new
     @current_team = Team.find(params[:team_id])
     @game = Game.new(requester: @current_team)
     authorize @game
+    if params[:city].blank?
     @teams = Team.all - [@current_team]
+    else
+      @teams = Team.where(city: params[:city].capitalize)
+    end
   end
 
   def create
