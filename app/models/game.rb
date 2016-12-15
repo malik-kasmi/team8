114e6a59ca91bcd9e919334aa32a5767048fc131
Malik Kasmi <malik.kasmi@gmail.com>
2016-12-15 12:29:22 +0100

    create seed

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :requester, class_name: "Team", foreign_key: 'team1_id'
   belongs_to :opponent, class_name: "Team", foreign_key: 'team2_id'
-  # belongs_to :winner, class_name: "Team", foreign_key: 'winner_id'
+  belongs_to :winner, class_name: "Team", foreign_key: 'winner_id', optional: true
 
   scope :upcoming, -> { where(status: ["pending", "accept"]) }
 end
